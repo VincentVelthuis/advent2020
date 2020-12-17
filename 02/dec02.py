5f6bf20b835cfdd20381d917f2e198aa5a26185d
Finz0id <v.velthuis@gmail.com>
2020-12-17 11:44:42 +0100

    .py file for part1

--- 02/dec02.py
+++ 02/dec02.py
@@ -1,11 +1,18 @@
-with open("test_input.csv", "r") as f:
-    list2 = []
-    for item in f: #split every line into chunks
+import sys
+
+valid_pws = 0
+
+with open(sys.argv[1], "r") as f:
+    for item in f: #split every line into usable chunks
         item_split_space=item.split(' ')
         n1 = int(item_split_space[0].split('-')[0].strip())
         n2 = int(item_split_space[0].split('-')[1].strip())
         c  = item_split_space[1].split(':')[0].strip()
         s  = item_split_space[2].strip()
+        c_in_s = s.count(c)
+        valid = n1 <= c_in_s <= n2
         
-        list2.append([n1,n2,c,s])
-print(list2)
+        if valid:
+            valid_pws += 1
+
+print(valid_pws)
