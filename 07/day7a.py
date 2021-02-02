c6ab4232a724dfb9d389f6f02a85a215c1c93875
Finz0id <v.velthuis@gmail.com>
2021-02-02 13:32:13 +0100

    part one solve

--- 07/day7a.py
+++ 07/day7a.py
@@ -1,14 +1,40 @@
 import sys,fileinput
 
-lines = [line.strip() for line in fileinput.input(files=sys.argv[1])]
+def input_to_dict(lines):
+    bags_dict = {}
+    for line in lines:
+        sentence = line.split(' ')
+        bag = "".join(sentence[:2])
+        bags_dict.setdefault(bag,[]) #to not get errors, every key needs to be initiated
+        content = sentence[4:]
+        if len(content) % 4 == 0:
+            for loc in range(0, len(content),4):
+                amount = content[loc]
+                child = "".join(content[loc+1:loc+3])
+                bags_dict[bag].append({"count":amount,"type":child})
+    return bags_dict
 
-for line in lines:
-    rule = line.split(' ')
-    bag = rule[:2]
-    contain = rule[4:]
-    if len(contain) % 4 == 0:
-        for content in range(0, len(contain),4):
-            print(contain[content:content+3])
+def bag_contains_shinygold(bags_dict, bag_name):
+    count = 0
+    bag = bags_dict[bag_name]
+    if len(bag) == 0:
+        return count
     else:
-        print("END")
-    print(bag,contain)
+        for sub_bag in bag:
+            if sub_bag["type"] == "shinygold":
+                count += 1
+            count += bag_contains_shinygold(bags_dict, sub_bag["type"])   
+    return count
+
+### START HERE
+# READ INPUT
+lines = [line.strip() for line in fileinput.input(files=sys.argv[1])]
+# CONVERT INPTUT TO DICT
+bags_dict = input_to_dict(lines)
+# COUNT BAGS CONTAINING AT LEAST ONE SHINYGOLD BAG
+count = 0
+for bag in bags_dict.keys():
+    if bag_contains_shinygold(bags_dict, bag) > 0:
+        count += 1
+
+print(count)
